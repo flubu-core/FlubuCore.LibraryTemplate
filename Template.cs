4082eacecd41932f0ef997c562ef1508691fdd8a
Marko Zorec <mzorec@gmail.com>
2020-10-30 07:47:27 +0100

    Replaces Template.json with Template.cs

---
+++ Template.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FlubuCore.Templating;
+
+namespace DotNet.Cli.Flubu
+{
+    public class Template : IFlubuTemplate
+    {
+        public void ConfigureTemplate(IFlubuTemplateBuilder templateBuilder)
+        {
+            templateBuilder.AddReplacementToken(new TemplateReplacmentToken()
+            {
+                Token = "{{SolutionFileName}}",
+                Description = "Enter relative path to solution filename:",
+            });
+        }
+    }
+}
